8edc8e023c59be3c0bc7fa31590f0098b41f444f
lgcarvalho <luizguilcarvalho@gmail.com>
2024-04-08 23:54:12 -0300

    Ajuste para gerar valor nulo na base de dados.

--- gerar_base_dados_seguro.py
+++ gerar_base_dados_seguro.py
@@ -1,22 +1,24 @@
 import csv
 import random
+import os
 
-# Função para gerar um valor aleatório para uma coluna específica
+# Função para gerar um valor aleatório para uma coluna específica, caso necessitar de alguma coluna em especifico com valor nulo adicionar o "None" na lista.
+# Para meus testes utilizei o IMC.
 def generate_random_value(column):
     if column == 'idade':
-        return random.randint(18, 80)
+        return random.choice([random.randint(18, 80)])
     elif column == 'gênero':
         return random.choice(['feminino', 'masculino'])
     elif column == 'imc':
-        return round(random.uniform(18.5, 40), 2)
+        return random.choice([round(random.uniform(18.5, 40), 2), None])
     elif column == 'filhos':
-        return random.randint(0, 5)
+        return random.choice([random.randint(0, 5)])
     elif column == 'fumante':
         return random.choice(['sim', 'não'])
     elif column == 'região':
         return random.choice(['nordeste', 'noroeste', 'sudeste', 'sudoeste'])
     elif column == 'encargos':
-        return round(random.uniform(10000, 50000), 2)
+        return random.choice([round(random.uniform(10000, 50000), 2)])
 
 # Lista das colunas do arquivo CSV
 columns = ['idade', 'gênero', 'imc', 'filhos', 'fumante', 'região', 'encargos']
@@ -24,8 +26,11 @@ columns = ['idade', 'gênero', 'imc', 'filhos', 'fumante', 'região', 'encargos'
 # Número de linhas a serem geradas
 num_rows = 5000
 
+# Diretório atual onde o script está sendo executado
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
 # Nome do arquivo CSV de saída
-output_file = 'seguro_medico.csv'
+output_file = os.path.join(current_directory, 'seguro_medico.csv')
 
 # Abrir o arquivo CSV para escrita
 with open(output_file, mode='w', newline='', encoding='utf-8-sig') as file:
